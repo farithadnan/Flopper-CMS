c25a12e4330966bf76ae2e15c8acdce647369229
Farith Syariffudin <22324133+ayiedfarith@users.noreply.github.com>
2020-11-11 19:41:33 +0800

    Update posts.php
    Previewing all the post data in the table in post.php's pages. will preview all the post that been created to the system.

--- admin/posts.php
+++ admin/posts.php
@@ -34,7 +34,44 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
+
+                                <?php
+
+    //find all posts query
+
+     $query = "SELECT * FROM posts "; //select all from table posts 
+                $select_posts = mysqli_query($connection, $query); //mysqli_query() use to simplify the use of performing query to db
+
+    while ($row = mysqli_fetch_assoc($select_posts)) 
+    { //amek and tukarkan column kepada key, and anak2 column as value dia s
+        $post_id = $row['post_id'];
+        $post_author = $row['post_author'];
+        $post_title = $row['post_title'];
+        $post_category_id = $row['post_category_id'];
+        $post_status = $row['post_status'];
+        $post_image = $row['post_image'];
+        $post_tag = $row['post_tag'];
+        $post_comment_count = $row['post_comment_count'];
+        echo $post_date = $row['post_date'];
+
+        echo "<tr>";
+        echo "<td> $post_id </td>";
+        echo "<td> $post_author </td>";
+        echo "<td> $post_title </td>";
+        echo "<td> $post_category_id </td>";
+        echo "<td> $post_status </td>";
+        echo "<td><img class='img-responsive' width='100' src='../images/$post_image' alt='images'>  </td>";
+        echo "<td> $post_tag </td>";
+        echo "<td> $post_comment_count </td>";
+        echo "<td> $post_date</td>";
+        echo "</tr>";
+
+
+    }
+
+
+
+                                  ?>
                                     <td>10</td>
                                     <td>Edwin</td>
                                     <td>Bootstrap Framework</td>
@@ -44,7 +81,6 @@
                                     <td>Tags</td>
                                     <td>Comments</td>
                                     <td>Date</td>
-                                </tr>
                             </tbody>
                         </table>
                     </div>
