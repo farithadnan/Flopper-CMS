0de7523c8d7728064f78fb877efdc6413bf58cf5
Farith Syariffudin <22324133+ayiedfarith@users.noreply.github.com>
2020-11-11 20:55:29 +0800

    Updating add_post.php
    learn on how to upload file with method post. Have to use enctype, and $_FILES superglobal and "move_uploaded_file"

---
+++ admin/includes/add_post.php
@@ -0,0 +1,72 @@
+<?php 
+
+if (isset($_POST['create_post'])) {
+
+	$post_title = $_POST['title'];
+	$post_author = $_POST['author'];
+	$post_category_id = $_POST['post_category_id'];
+	$post_status = $_POST['post_status'];
+
+
+	//user superglobal $_FILES to send data thru post
+	$post_image = $_FILES['image']['name']; // image the name of the file input
+	$post_image_temp = $_FILES['image']['tmp_name']; // temporary files, when previewing the name of the file, this also needed to be transfer
+
+	$post_tags = $_POST['post_tags'];
+	$post_content = $_POST['post_content'];
+	$post_date = date('d-m-y'); //using default date function, with a format to capture date
+	$post_comment_count = 4;
+
+
+	move_uploaded_file($post_image_temp, "../images/$post_image"); 
+
+
+}
+ ?>
+
+
+
+<!-- enctype multipart/form-data is require if u want to send file thru post-->
+<form action="" method="post" enctype="multipart/form-data">
+
+	<div class="form-group">
+		<label for="title">Post Title</label>
+		<input type="text" class="form-control" name="title">
+	</div>
+
+	<div class="form-group">
+		<label for="post_category">Post Category</label>
+		<input type="text" class="form-control" name="post_category_id">
+	</div>	
+
+	<div class="form-group">
+		<label for="author">Post Author</label>
+		<input type="text" class="form-control" name="author">
+	</div>
+
+	<div class="form-group">
+		<label for="post_status">Post Status</label>
+		<input type="text" class="form-control" name="post_status">
+	</div>
+
+	<div class="form-group">
+		<label for="image">Post Image</label>
+		<input type="file" class="form-control" name="image">
+	</div>
+
+	<div class="form-group">
+		<label for="post_tags">Post Tags</label>
+		<input type="text" class="form-control" name="post_tags">
+	</div>
+
+	<div class="form-group">
+		<label for="post_tags">Post Content</label>
+		<textarea type="text" class="form-control" name="post_content" id="" cols="30" rows="10">
+		</textarea>
+	</div>
+
+	<div class="form-group">
+		<input type="submit" class="btn btn-primary" name="create_post" value="Publish Post">
+	</div>
+
+</form>
