f45a17c80523344998084f0651b98dd35ca8dfc5
Farith Syariffudin <22324133+ayiedfarith@users.noreply.github.com>
2020-11-11 20:00:06 +0800

    previewing different ouput, but using only 1 page instead of multiple page
    For this update, i learnt how to preview multiple ouput (view) by only using one page, for this method it rely on get request, which can pretty handy when making CRUDS, we don't multiple pages for edit, update, delete, we only need to use $_get and only 1 page

---
+++ admin/includes/view_all_posts.php
@@ -0,0 +1,62 @@
+                        <table class="table table-bordered table-hover table-sm">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Status</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <?php
+
+    //find all posts query
+
+     $query = "SELECT * FROM posts "; //select all from table posts 
+                $select_posts = mysqli_query($connection, $query); //mysqli_query() use to simplify the use of performing query to db
+
+    while ($row = mysqli_fetch_assoc($select_posts)) 
+    { //amek and tukarkan column kepada key, and anak2 column as value dia s
+        $post_id = $row['post_id'];
+        $post_author = $row['post_author'];
+        $post_title = $row['post_title'];
+        $post_category_id = $row['post_category_id'];
+        $post_status = $row['post_status'];
+        $post_image = $row['post_image'];
+        $post_tag = $row['post_tag'];
+        $post_comment_count = $row['post_comment_count'];
+        echo $post_date = $row['post_date'];
+
+        echo "<tr>";
+        echo "<td> $post_id </td>";
+        echo "<td> $post_author </td>";
+        echo "<td> $post_title </td>";
+        echo "<td> $post_category_id </td>";
+        echo "<td> $post_status </td>";
+        echo "<td><img class='img-responsive' width='100' src='../images/$post_image' alt='images'>  </td>";
+        echo "<td> $post_tag </td>";
+        echo "<td> $post_comment_count </td>";
+        echo "<td> $post_date</td>";
+        echo "</tr>";
+
+
+    }
+                                  ?>
+                                    <td>10</td>
+                                    <td>Edwin</td>
+                                    <td>Bootstrap Framework</td>
+                                    <td>Bootstrap</td>
+                                    <td>Status</td>
+                                    <td>Image</td>
+                                    <td>Tags</td>
+                                    <td>Comments</td>
+                                    <td>Date</td>
+                            </tbody>
+                        </table>
