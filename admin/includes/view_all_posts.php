35e9c4506e0acd59356d1ed98b3da9575dd569e3
Ayied Farith <22324133+ayiedfarith@users.noreply.github.com>
2020-11-15 22:34:10 +0800

    Update view_all_posts.php
    trying to relate the post_category_id from table post with cat_id from table category; this means, instead of displaying the number to represent the category, instead it will previewing the category title, which is the correct and most logic way.

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -39,7 +39,32 @@
         echo "<td> $post_id </td>";
         echo "<td> $post_author </td>";
         echo "<td> $post_title </td>";
-        echo "<td> $post_category_id </td>";
+
+
+
+        //THIS ONE WILL RELATE THE POST CATEGORY ID FROM TABLE POST WITH CAT ID IN TABLE CATEGORIES
+         $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id} ";
+         $select_categories_id = mysqli_query($connection, $query); 
+
+         while ($row = mysqli_fetch_assoc( $select_categories_id )) { 
+         $cat_id = $row['cat_id'];
+         $cat_title = $row['cat_title'];
+
+
+
+
+
+
+        echo "<td> {$cat_title} </td>";
+
+        }
+
+
+
+
+
+
+
         echo "<td> $post_status </td>";
         echo "<td><img class='img-responsive' width='100' src='../images/$post_image' alt='images'>  </td>";
         echo "<td> $post_tag </td>";
