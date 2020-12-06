4bd313bd6b1826d7ab5dd7372e409e6efe1aea10
Farith Syariffudin <22324133+ayiedfarith@users.noreply.github.com>
2020-12-06 22:26:19 +0800

    simple login function for cms

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,7 +1,18 @@
 <?php include("../includes/db.php");?>
 <?php include("functions.php"); ?>
 <?php ob_start(); ?>
+<?php session_start(); ?>
 
+<?php 
+    if(!isset($_SESSION['user_role']))
+    {
+        header("Location: ../index.php");     
+    }
+    else
+    {
+
+    }
+ ?>
 
 
 <!DOCTYPE html>
