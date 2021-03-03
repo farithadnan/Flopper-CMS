94570b563a7291bd1353249a59d0e4fe7e464bfe
Farith Syariffudin <22324133+ayiedfarith@users.noreply.github.com>
2021-03-04 03:39:02 +0800

    Update cms.sql

--- cms.sql
+++ cms.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2021 at 11:58 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.11
+-- Generation Time: Mar 03, 2021 at 08:38 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -67,11 +67,10 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`comment_id`, `comment_post_id`, `comment_author`, `comment_email`, `comment_content`, `comment_status`, `comment_date`) VALUES
-(3, 3, 'ayied', 'ayied@gmail.com', 'test comment', 'Approved', '2020-11-22'),
-(4, 3, 'holla', 'holla@gmail.com', 'dadadadada', 'Approved', '2020-11-24'),
-(8, 3, 'dummy', 'dummy@gmail.com', 'fulamak memang nice', 'Approved', '2021-01-28'),
-(9, 3, 'aaaa', 'ayied@gmail.com', 'adadada', 'Approved', '2021-02-01'),
-(10, 3, 'aaaa', 'ayied@gmail.com', 'a new comment!', 'unapproved', '2021-02-01');
+(4, 3, 'holla', 'holla@gmail.com', 'dadadadada', 'Unapproved', '2020-11-24'),
+(8, 3, 'dummy', 'dummy@gmail.com', 'fulamak memang nice', 'Unapproved', '2021-01-28'),
+(9, 3, 'aaaa', 'ayied@gmail.com', 'adadada', 'Unapproved', '2021-02-01'),
+(10, 3, 'aaaa', 'ayied@gmail.com', 'a new comment!', 'Unapproved', '2021-02-01');
 
 -- --------------------------------------------------------
 
@@ -98,7 +97,7 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`post_id`, `post_category_id`, `post_title`, `post_author`, `post_date`, `post_image`, `post_content`, `post_tag`, `post_comment_count`, `post_status`, `post_view_count`) VALUES
-(3, 2, 'Javascript', 'Edwin Diaz', '2021-01-28', 'Mount Fuji.jpg', '<p>This course is great!</p>', 'Javascript, courses, classes, great', 8, 'Published', 1),
+(3, 2, 'Javascript', 'Edwin Diaz', '2021-01-28', 'Mount Fuji.jpg', '<p>This course is great!</p>', 'Javascript, courses, classes, great', 9, 'Published', 0),
 (4, 3, 'PHP', 'Payed', '2021-01-28', 'php.png', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultricies mi non purus rhoncus, mollis pretium mauris tristique. Vivamus consequat in neque sed porta. Mauris dignissim blandit sem in gravida. Vivamus in mi nec sem rhoncus malesuada nec eget eros. Mauris eleifend pharetra ultricies. Nullam tellus nulla, consequat in enim quis, sodales finibus magna. Pellentesque fermentum velit nec leo venenatis porttitor. Donec consectetur elit et sem convallis tincidunt. Integer et ligula vel ex sodales ultrices. Etiam iaculis feugiat enim sit amet scelerisque. In magna metus, rhoncus eu justo nec, cursus volutpat neque. Nulla ullamcorper suscipit molestie. Aenean hendrerit augue ex, vel congue libero pretium eget. Fusce fermentum enim eu ex cursus, ac laoreet nisi mollis. Quisque semper nec felis vitae laoreet. Morbi rutrum lorem ut diam feugiat tempor. Nullam ullamcorper lacinia libero et sagittis. Vivamus nec tempus erat. Quisque euismod purus nisl, eu lacinia tortor imperdiet ut. Nulla congue bibendum lacus ut placerat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis sed sapien varius, lacinia diam quis, facilisis urna. Mauris bibendum pharetra neque, ut mattis tortor sodales non.</p>', 'php, ', 4, 'Published', 0),
 (6, 2, 'Another post', 'Payed', '2021-01-27', 'sophia.jfif', '<p>twatssss</p>', 'php, laravel', 0, 'Published', 0),
 (9, 3, 'TEST', 'ricos', '2021-01-28', 'ann tamaki p5.jpg', '<p>dadadad</p>', 'php, ', 0, 'Published', 0),
@@ -169,18 +168,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `user_password`, `user_firstname`, `user_lastname`, `user_email`, `user_image`, `user_role`) VALUES
-(1, 'ricos', '$2y$12$ZwjWXrA9ANxWVLZOSId9xeetleqWYzRXN4rHth6JKilsyUp/5dD5m', 'Ricos', 'Suave', 'ricosuave@gmail.com', '', 'subscriber'),
-(2, 'peterjackson', '123', 'Peter', 'Jackson Bossku', 'williampeterson@gmail.com', '', 'Subscriber'),
-(5, 'ayiedfarith', 'Jk102a01789', 'Farith', 'Syariffudin', 'ayiedfarith@gmial.com', '', 'Subscriber'),
-(6, 'jacky', '123', 'jack', 'sparrow', 'jacky@gmail.com', '', 'Subscriber'),
-(7, 'razak', '123', 'Abdul', 'Razak', 'razak@gmail.com', '', 'Subscriber'),
-(8, 'aminah', '123', 'Aminah', 'Albert', 'aminah@gmail.com', '', 'Subscriber'),
-(9, 'aiman', '123', 'Aiman', 'Salman', 'aimansalman@pm.me', '', 'Subscriber'),
-(11, 'whatever', '123', 'erwin', 'edmund', 'whatever@gmail.com', '', 'Subscriber'),
-(13, 'demon', '123', 'demon', 'petersonsss', 'demon@gmail.com', '', 'Subscriber'),
+(1, 'ricos', '$2y$12$9E.61vrjGIesieVquqgDMuoA7LDqVkRA98N3X1qwaLWaZNNDlgjzS', 'Ricosdd', 'Suavedd', 'ricosuave@gmail.com', '', 'Admin'),
 (18, 'admin', '$1$baa6QDmE$Lg6R0XMF/HcErV6vXv6ns1', 'master', 'admin', 'admin@pm.me', '', 'Subscriber'),
-(20, 'new_user', '$2y$12$2c1E3wlAsv0pII2uBkoCguxRKnofj8iOi6R1jzzBhHlUtjdyjAs0.', '', '', 'support@gmail.com', '', 'Admin '),
-(21, 'new_user_2', '$2y$12$v6v43dGF7bHmTU8BQTo4q.9t1l/r4/rJRuH3n47boauXniJDjP4KW', 'newuser', '2', 'support2@gmail.com', '', 'Subscriber');
+(21, 'new_user_2', '$2y$12$v6v43dGF7bHmTU8BQTo4q.9t1l/r4/rJRuH3n47boauXniJDjP4KW', 'newuser', '2', 'support2@gmail.com', '', 'Subscriber'),
+(22, 'basic', '$2y$12$OD8aOQ94KNqnA1cPBGkbbukBQOxJgMmjWlQkZwVFXZGYGkOdjrlxu', 'basic', 'user', 'basic@yahoo.com', '', 'subscriber');
 
 -- --------------------------------------------------------
 
@@ -201,7 +192,9 @@ CREATE TABLE `user_online` (
 INSERT INTO `user_online` (`id`, `session`, `time_log`) VALUES
 (7, 'up9c5nb6o12eh82ke3svejgb9n', 1613849546),
 (8, 'apf6bmrgp67b6maagh8evhb2ka', 1612978031),
-(9, 'd5tpmm7do9mpjke8ct71j3210d', 1612978184);
+(9, 'd5tpmm7do9mpjke8ct71j3210d', 1612978184),
+(10, '8g9od61k86hfa8jojm7j9gf9um', 1614800222),
+(11, 'g33g7m4mv82rredh34jispllb8', 1614792222);
 
 --
 -- Indexes for dumped tables
@@ -251,7 +244,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `comment_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `posts`
@@ -263,13 +256,13 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `user_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `user_online`
 --
 ALTER TABLE `user_online`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
